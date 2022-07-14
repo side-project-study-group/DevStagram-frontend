8d9f4f09991d7e5a5fa4ecfe3c604e29139bb5e2
Emily <yday1223@gmail.com>
2022-07-14 21:13:34 +0900

    [기타] CSS관련 파일

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import './assets/fonts/Lean.otf';
+import './assets/fonts/NotoSansKR-Bold.otf';
+import './assets/fonts/NotoSansKR-Regular.otf';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
