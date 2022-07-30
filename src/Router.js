a317a772f5ddfff1ef2e5f35dc1ed5c077e53f80
Emily <yday1223@gmail.com>
2022-07-30 18:42:50 +0900

    [기타] font관련 수정사항

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,23 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/organisms/header";
-import MeetUpChatList from "./pages/meet-up-chat-list/meet-up-chat-list";
-import MeetUpList from "./pages/meet-up-list/meet-up-list";
-import MeetUpManage from "./pages/meet-up-manage/meet-up-manage";
-import MeetUpPost from "./pages/meet-up-post/meet-up-post";
-import MeetUpWrite from "./pages/meet-up-write/meet-up-write";
-import TimeLineDetail from "./pages/timeline-detail/timeline-detail"
- 
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/organisms/header';
+import MeetUpChatList from './pages/meet-up-chat-list/meet-up-chat-list';
+import MeetUpList from './pages/meet-up-list/meet-up-list';
+import MeetUpManage from './pages/meet-up-manage/meet-up-manage';
+import MeetUpPost from './pages/meet-up-post/meet-up-post';
+import MeetUpWrite from './pages/meet-up-write/meet-up-write';
+
 function Router() {
   return (
     <>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<MeetUpList />} />
-          <Route path="/meet-up-post" element={<MeetUpPost />} />
-          <Route path="/meet-up-chat-list" element={<MeetUpChatList />} />
-          <Route path="/meet-up-write" element={<MeetUpWrite />} />
-          <Route path="/meet-up-manage" element={<MeetUpManage />} />
-          <Route path="/timeline-detail" element={<TimeLineDetail />} />
+          <Route path='/' element={<MeetUpList />} />
+          <Route path='/meet-up-tab' element={<MeetUpPost />} />
+          <Route path='/meet-up-chat-list' element={<MeetUpChatList />} />
+          <Route path='/new-meet-up' element={<MeetUpWrite />} />
+          <Route path='/meet-up-manage' element={<MeetUpManage />} />
         </Routes>
       </BrowserRouter>
     </>
