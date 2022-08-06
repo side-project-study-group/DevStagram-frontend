ee74f0741e777e8e37c14cc270d5a5b25ec90c5d
Emily <yday1223@gmail.com>
2022-08-06 21:20:54 +0900

    [컴포넌트] 밋업 채팅 리스트 페이지 수정

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/organisms/header';
 import HeaderBack from './components/organisms/header/header-back';
 import HeaderHome from './components/organisms/header/header-home';
 import MeetUpChatList from './pages/meet-up-chat-list/meet-up-chat-list';
 import MeetUpList from './pages/meet-up-list/meet-up-list';
-import MeetUpManage from './pages/meet-up-manage/meet-up-manage';
-import MeetUpPost from './pages/meet-up-post/meet-up-post';
 import MeetUpWrite from './pages/meet-up-write/meet-up-write';
+import MeetUpDetail from './pages/meet-up-detail/meet-up-detail';
 
 function Router() {
   const [pathName, setPathName] = useState('/');
@@ -22,10 +20,9 @@ function Router() {
         {pathName === '/' ? <HeaderHome /> : <HeaderBack />}
         <Routes>
           <Route path='/' element={<MeetUpList />} />
-          <Route path='/meet-up-tab' element={<MeetUpPost />} />
+          <Route path='/meet-up-detail' element={<MeetUpDetail />} />
           <Route path='/meet-up-chat-list' element={<MeetUpChatList />} />
-          <Route path='/new-meet-up' element={<MeetUpWrite />} />
-          <Route path='/meet-up-manage' element={<MeetUpManage />} />
+          <Route path='/meet-up-write' element={<MeetUpWrite />} />
         </Routes>
       </BrowserRouter>
     </>
