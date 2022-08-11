f3b42bf8fd93bec82e975c06554036bbbae14d81
Emily <yday1223@gmail.com>
2022-08-11 19:26:27 +0900

    [컴포넌트] 버튼 클릭 기능 추가

--- src/components/atoms/plus-button.js
+++ src/components/atoms/plus-button.js
@@ -1,70 +1,51 @@
-import React from "react";
-import styled from "styled-components";
-import { BsPlusLg } from "react-icons/bs";
+import React from 'react'
+import styled from 'styled-components'
+import { BsPlusLg } from 'react-icons/bs'
 
 const Wrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`;
-
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  margin: 0 auto;
-  max-width: 480px;
-  display: flex;
-  justify-content: end;
-  align-items: flex-end;
-  box-sizing: border-box;
-`;
+    width: 100%;
+    max-width: 480px;
+    margin: 0 auto;
+`
 
 const BtnContainer = styled.div`
-  z-index: 99;
-  margin-right: 30px;
-  margin-bottom: 50px;
-  position: fixed;
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  background-color: #fab923;
-  border: 2px solid #414042;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-shadow: 2px 3px 0px rgba(0, 0, 0, 0.1);
-  button {
-    font-size: 25px;
-    background-color: transparent;
-    border: none;
-    color: #414042;
-    padding: 0;
+    z-index: 99;
+    position: fixed;
+    margin-left: 380px;
+    bottom: 50px;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border: 2px solid #414042;
     display: flex;
+    justify-content: center;
     align-items: center;
-  }
-  :hover {
-    background-color: #ffffff;
+    background-color: ${(props) =>
+        props.isOpenPopUp ? '#ffffff' : ' #fab923'};
+    box-shadow: 2px 3px 0px rgba(0, 0, 0, 0.1);
     button {
-      transition: all 0.5s ease-in-out;
-      transform: rotate(45deg);
+        font-size: 25px;
+        background-color: transparent;
+        border: none;
+        color: #414042;
+        padding: 0;
+        display: flex;
+        align-items: center;
+        transition: all 0.3s ease-in-out;
+        transform: ${(props) => (props.isOpenPopUp ? 'rotate(45deg)' : 'none')};
     }
-  }
-`;
+`
 
-function PlusButton() {
-  return (
-    <Wrapper>
-      <Container>
-        <BtnContainer>
-          <button>
-            <BsPlusLg />
-          </button>
-        </BtnContainer>
-      </Container>
-    </Wrapper>
-  );
+function PlusButton({ isOpenPopUp, handleClick }) {
+    return (
+        <Wrapper>
+            <BtnContainer isOpenPopUp={isOpenPopUp}>
+                <button onClick={handleClick}>
+                    <BsPlusLg />
+                </button>
+            </BtnContainer>
+        </Wrapper>
+    )
 }
 
-export default PlusButton;
+export default PlusButton
