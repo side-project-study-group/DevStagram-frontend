58df3d0f375750136124d97881c4ed9dfaa11e07
YDayeon <88532969+YDayeon@users.noreply.github.com>
2022-08-12 17:45:02 +0900

    [컴포넌트] header navigate 기능 추가

--- src/components/organisms/header/header-back.js
+++ src/components/organisms/header/header-back.js
@@ -1,42 +1,51 @@
-import React from 'react';
-import styled from 'styled-components';
-import { BsChatSquareText } from 'react-icons/bs';
-import { VscHome } from 'react-icons/vsc';
-import { HiOutlineChatAlt2 } from 'react-icons/hi';
-import { BsPerson } from 'react-icons/bs';
-import { FiTriangle } from 'react-icons/fi';
-import { RiListSettingsLine } from 'react-icons/ri';
+import React from 'react'
+import styled from 'styled-components'
+import Back from '../../../assets/icons/Back.svg'
+import Control from '../../../assets/icons/Control.svg'
+import { useNavigate } from 'react-router-dom'
 
 const Container = styled.div`
-  background-color: #fcfcfa;
-  width: 100%;
-  height: 50px;
-  max-width: 480px;
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-around;
-  border-bottom: 2px solid #24231f;
-  padding: 0 30px;
-  box-sizing: border-box;
-  .icon_back,
-  .icon_control {
-    font-size: 35px;
-    margin: auto 0;
-  }
-  .icon_back {
-    transform: rotate(-90deg);
-    fill: rgba(250, 215, 120, 1);
-    color: rgba(65, 64, 66, 1);
-  }
-`;
+    background-color: #fcfcfa;
+    width: 100%;
+    height: 50px;
+    max-width: 480px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 2px solid #24231f;
+    padding: 0 30px;
+    box-sizing: border-box;
+    .icon_back,
+    .icon_control {
+        font-size: 35px;
+        margin: auto 0;
+    }
+    .icon_back {
+        transform: rotate(-90deg);
+        fill: rgba(250, 215, 120, 1);
+        color: rgba(65, 64, 66, 1);
+    }
+`
+
+const Img = styled.img`
+    width: 30px;
+    cursor: pointer;
+`
+
+const ImgBack = styled.img`
+    width: 26px;
+    cursor: pointer;
+`
 
 function HeaderBack() {
-  return (
-    <Container>
-      <FiTriangle className='icon_back' />
-      <RiListSettingsLine className='icon_control' />
-    </Container>
-  );
+    const navigate = useNavigate()
+
+    return (
+        <Container>
+            <ImgBack src={Back} onClick={() => navigate(-1)} />
+            <Img src={Control} />
+        </Container>
+    )
 }
 
-export default HeaderBack;
+export default HeaderBack
--- src/components/organisms/header/header-home.js
+++ src/components/organisms/header/header-home.js
@@ -1,44 +1,39 @@
-import React from 'react';
-import styled from 'styled-components';
-import { BsChatSquareText } from 'react-icons/bs';
-import { VscHome } from 'react-icons/vsc';
-import { HiOutlineChatAlt2 } from 'react-icons/hi';
-import { BsPerson } from 'react-icons/bs';
+import React from 'react'
+import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
+import Home from '../../../assets/icons/Home.svg'
+import TimeLine from '../../../assets/icons/TimeLine.svg'
+import Profile from '../../../assets/icons/Profile.svg'
+import Chat from '../../../assets/icons/Chat.svg'
 
 const Container = styled.div`
-  background-color: #fcfcfa;
-  width: 100%;
-  height: 50px;
-  max-width: 480px;
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 2px solid #24231f;
-  padding: 0 30px;
-  box-sizing: border-box;
-  .icon_home,
-  .icon_chat,
-  .icon_comment,
-  .icon_profile {
-    font-size: 35px;
-    margin: auto 0;
-  }
-  .icon_back {
-    transform: rotate(-90deg);
-    fill: rgba(250, 215, 120, 1);
-    color: rgba(65, 64, 66, 1);
-  }
-`;
+    background-color: #fcfcfa;
+    width: 100%;
+    height: 50px;
+    max-width: 480px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 2px solid #24231f;
+    padding: 0 30px;
+    box-sizing: border-box;
+`
+
+const Img = styled.img`
+    cursor: pointer;
+    width: 30px;
+`
 
 function HeaderHome() {
-  return (
-    <Container>
-      <VscHome className='icon_home' />
-      <BsChatSquareText className='icon_chat' />
-      <HiOutlineChatAlt2 className='icon_comment' />
-      <BsPerson className='icon_profile' />
-    </Container>
-  );
+    const navigate = useNavigate()
+    return (
+        <Container>
+            <Img src={Home} onClick={() => navigate('/')} />
+            <Img src={TimeLine} />
+            <Img src={Chat} onClick={() => navigate('/meet-up-chat-list')} />
+            <Img src={Profile} onClick={() => navigate('/my-profile-post')} />
+        </Container>
+    )
 }
 
-export default HeaderHome;
+export default HeaderHome
