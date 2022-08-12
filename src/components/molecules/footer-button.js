7ed2866ff43afeb5c605bca7c7f5f2bc880759cf
YDayeon <88532969+YDayeon@users.noreply.github.com>
2022-08-12 17:29:32 +0900

    [컴포넌트] footer 버튼 수정
    - CSS width 수정
    - props 추가 (handleClick)
    - onClick 기능 추가

--- src/components/molecules/footer-button.js
+++ src/components/molecules/footer-button.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
 const Footer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  margin-top: 70px;
-`;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`
 
 const Container = styled.div`
-  box-sizing: border-box;
-  max-width: 480px;
-  padding: 0 10px;
-  bottom: 0;
-`;
+    position: fixed;
+    box-sizing: border-box;
+    max-width: 480px;
+    bottom: 25px;
+`
 
 const Button = styled.div`
-  padding: 0 60px;
-  height: 40px;
-  line-height: 40px;
-  background-color: rgba(75, 100, 230, 1);
-  border-radius: 20px;
-  text-align: center;
-  border: 2px solid rgba(65, 64, 66, 1);
-  color: #ffffff;
-  font-family: 'NotoSansKRBold';
-`;
+    cursor: pointer;
+    width: 200px;
+    height: 40px;
+    line-height: 40px;
+    background-color: rgba(75, 100, 230, 1);
+    border-radius: 20px;
+    text-align: center;
+    border: 2px solid rgba(65, 64, 66, 1);
+    color: #ffffff;
+    font-family: 'NotoSansKRBold';
+`
 
-function FooterBtn({ text }) {
-  return (
-    <Footer>
-      <Container>
-        <Button>{text}</Button>
-      </Container>
-    </Footer>
-  );
+function FooterBtn({ text, handleClick }) {
+    return (
+        <Footer>
+            <Container>
+                <Button onClick={handleClick}>{text}</Button>
+            </Container>
+        </Footer>
+    )
 }
 
-export default FooterBtn;
+export default FooterBtn
