989da38ac9282d96273343d33853af28838477e3
Emily <yday1223@gmail.com>
2022-08-14 18:41:20 +0900

    [컴포넌트] footerbutton 폴더 변경
    footerbutton 컴포넌트 atom 폴더로 이동

--- src/pages/meet-up-chat-list/meet-up-chat-list-template.js
+++ src/pages/meet-up-chat-list/meet-up-chat-list-template.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import styled from 'styled-components';
-import FooterBtn from '../../components/molecules/footer-button';
-import MeetUpChatBox from '../../components/organisms/meet-up-chat-box';
-import PopUp from '../../components/organisms/pop-up';
+import React from 'react'
+import styled from 'styled-components'
+import FooterBtn from '../../components/atoms/footer-button'
+import MeetUpChatBox from '../../components/organisms/meet-up-chat-box'
+import PopUp from '../../components/organisms/pop-up'
 
 const Section = styled.section`
-  width: 100%;
-  margin: 0 auto;
-  max-width: 480px;
-  background-color: rgba(250, 250, 248, 1);
-`;
+    width: 100%;
+    margin: 0 auto;
+    max-width: 480px;
+    background-color: rgba(250, 250, 248, 1);
+`
 
 const Ul = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-`;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+`
 
 function MeetUpChatListTemp() {
-  return (
-    <Section>
-      <Ul>
-        <MeetUpChatBox />
-        <MeetUpChatBox />
-        <MeetUpChatBox />
-        <MeetUpChatBox />
-        <MeetUpChatBox />
-        {/* <PopUp /> */}
-        <FooterBtn text={'수정하기'} />
-      </Ul>
-    </Section>
-  );
+    return (
+        <Section>
+            <Ul>
+                <MeetUpChatBox />
+                <MeetUpChatBox />
+                <MeetUpChatBox />
+                <MeetUpChatBox />
+                <MeetUpChatBox />
+                {/* <PopUp /> */}
+                <FooterBtn text={'수정하기'} />
+            </Ul>
+        </Section>
+    )
 }
 
-export default MeetUpChatListTemp;
+export default MeetUpChatListTemp
--- src/pages/meet-up-detail/meet-up-detail-templete.js
+++ src/pages/meet-up-detail/meet-up-detail-templete.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import styled from 'styled-components';
-import MeetUpPostBox from '../../components/meet-up-post-box';
-import FooterBtn from '../../components/molecules/footer-button';
-import PopUp from '../../components/organisms/pop-up';
+import React from 'react'
+import styled from 'styled-components'
+import MeetUpPostBox from '../../components/meet-up-post-box'
+import FooterBtn from '../../components/atoms/footer-button'
+import PopUp from '../../components/organisms/pop-up'
 
 const Section = styled.section`
-  width: 100%;
-  height: 100%;
-  background-color: rgba(250, 250, 248, 1);
-  max-width: 480px;
-  margin: 0 auto;
-  padding: 10px 10px;
-  box-sizing: border-box;
-`;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(250, 250, 248, 1);
+    max-width: 480px;
+    margin: 0 auto;
+    padding: 10px 10px;
+    box-sizing: border-box;
+`
 
 function MeetUpPostTemp() {
-  return (
-    <Section>
-      <MeetUpPostBox size={'big'} />
-      <FooterBtn text='참여하기' />
-      <PopUp />
-    </Section>
-  );
+    return (
+        <Section>
+            <MeetUpPostBox size={'big'} />
+            <FooterBtn text="참여하기" />
+            <PopUp />
+        </Section>
+    )
 }
 
-export default MeetUpPostTemp;
+export default MeetUpPostTemp
