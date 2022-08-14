f9446b066a9a813536c051da39de8fa7b4418ef0
Emily <yday1223@gmail.com>
2022-08-14 18:38:00 +0900

    [컴포넌트] meet-up-category-tag
    컴포넌트명 및 파일명을 "tag"에서 "meet-up-category-tag", "MeetUpCategoryTag"로 변경

--- src/components/meet-up-post-box.js
+++ src/components/meet-up-post-box.js
@@ -1,79 +1,79 @@
-import React from 'react';
-import styled from 'styled-components';
-import MemberTag from './molecules/member-tag';
-import MeetUpTitle from './molecules/meet-up-title';
-import Tag from './atoms/tag';
-import TextBox from './atoms/text-box';
-import ProfileTag from './molecules/profile-tag';
-import { ImLock } from 'react-icons/im';
-import ProfileImg from './atoms/profile-img';
-import TimeLine from './atoms/time-line';
+import React from 'react'
+import styled from 'styled-components'
+import MemberTag from './molecules/member-tag'
+import MeetUpTitle from './molecules/meet-up-title'
+import TextBox from './atoms/text-box'
+import ProfileTag from './molecules/profile-tag'
+import { ImLock } from 'react-icons/im'
+import ProfileImg from './atoms/profile-img'
+import TimeLine from './atoms/time-line'
+import MeetUpCategroyTag from './atoms/meet-up-category-tag'
 
 const Section = styled.div`
-  margin-bottom: 10px;
-  box-sizing: border-box;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  position: relative;
-  background-color: rgba(255, 255, 255, 1);
-  border: 2px solid #24231f;
-  border-radius: 10px;
-  max-width: 480px;
-  padding: 10px;
-`;
+    margin-bottom: 10px;
+    box-sizing: border-box;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+    background-color: rgba(255, 255, 255, 1);
+    border: 2px solid #24231f;
+    border-radius: 10px;
+    max-width: 480px;
+    padding: 10px;
+`
 
 const Header = styled.header`
-  display: flex;
-`;
+    display: flex;
+`
 
 const Wrapper = styled.div`
-  width: 100%;
-`;
+    width: 100%;
+`
 
 const EditBtn = styled.div`
-  position: absolute;
-  right: 10px;
-  .edit_icon {
-    width: 25px;
-    height: 25px;
-  }
-`;
+    position: absolute;
+    right: 10px;
+    .edit_icon {
+        width: 25px;
+        height: 25px;
+    }
+`
 
 const Footer = styled.footer`
-  width: 100%;
-  display: flex;
-`;
+    width: 100%;
+    display: flex;
+`
 
 const Container = styled.div`
-  display: flex;
-  align-items: center;
-`;
+    display: flex;
+    align-items: center;
+`
 
 function MeetUpPostBox({ children, size }) {
-  return (
-    <Section>
-      <Wrapper>
-        <Header>
-          <ImLock style={{ color: 'green' }} />
-          <Tag text='프로젝트' />
-        </Header>
-        <Container>
-          <ProfileTag size={size} />
-          <TimeLine />
-        </Container>
-        <EditBtn>{children}</EditBtn>
-        <MeetUpTitle />
-        <TextBox />
-        <Footer>
-          <MemberTag />
-          <ProfileImg size='small' />
-          <ProfileImg size='small' />
-          <ProfileImg size='small' />
-        </Footer>
-      </Wrapper>
-    </Section>
-  );
+    return (
+        <Section>
+            <Wrapper>
+                <Header>
+                    <ImLock style={{ color: 'green' }} />
+                    <MeetUpCategroyTag text="프로젝트" />
+                </Header>
+                <Container>
+                    <ProfileTag size={size} />
+                    <TimeLine />
+                </Container>
+                <EditBtn>{children}</EditBtn>
+                <MeetUpTitle />
+                <TextBox />
+                <Footer>
+                    <MemberTag />
+                    <ProfileImg size="small" />
+                    <ProfileImg size="small" />
+                    <ProfileImg size="small" />
+                </Footer>
+            </Wrapper>
+        </Section>
+    )
 }
 
-export default MeetUpPostBox;
+export default MeetUpPostBox
