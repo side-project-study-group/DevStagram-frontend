a8817ab1411100335cc4e2f8a5ea4690d45a8ab7
sunho-lee <sh112123@naver.com>
2022-08-23 22:23:10 +0900

    [기능] 태그 중복 추가 방지 구현

--- src/hooks/useTag.js
+++ src/hooks/useTag.js
@@ -1,19 +1,31 @@
 import { useState } from 'react'
 
 function useTag() {
-    const [text, setText] = useState('')
+    const [value, setValue] = useState('')
     const [tags, setTags] = useState([])
 
     const handleChange = (e) => {
         const reg = /\r\n|\n|\r|\s|\t/gm
-        setText(e.target.value)
-        if (e.target.value.match(reg)) {
-            const space = e.target.value.replace(reg, '')
-            if (space !== '') {
-                setTags([...tags, e.target.value.replace(reg, '')])
-            }
-            setText('')
+        // 공백만 있을 경우 함수 종료
+        if (e.target.value === ' ') return
+
+        // 인풋에 value로 넣을 값
+        setValue(e.target.value)
+
+        // 스페이스바 등 공백 없을 경우 함수 종료
+        if (!e.target.value.match(reg)) return
+
+        //텍스트 중간에서 공백을 넣는 예외시 공백 삭제 처리
+        const tag = e.target.value.replace(reg, '')
+
+        //기존에 추가한 태그인 경우 함수 종료
+        if (tags.includes(tag)) {
+            setValue('')
+            return
         }
+        //태그에 추가
+        setTags([...tags, tag])
+        setText('')
     }
 
     const handleClick = (e) => {
@@ -21,7 +33,7 @@ function useTag() {
         setTags(tags.filter((item) => item !== name))
     }
 
-    return [text, tags, handleChange, handleClick]
+    return [value, tags, handleChange, handleClick]
 }
 
 export default useTag
