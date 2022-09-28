98f37592f4dcadc5aebbc86be151ce955517b269
YDayeon <88532969+YDayeon@users.noreply.github.com>
2022-09-28 09:08:58 +0900

    [컴포넌트] button data 파일 key값 수정

--- src/components/organisms/modal/bottom-pop-up/button-data.js
+++ src/components/organisms/modal/bottom-pop-up/button-data.js
@@ -2,7 +2,7 @@ import Delete from '../../../../assets/icons/Delete.svg'
 import Edit from '../../../../assets/icons/Edit.svg'
 
 export const button_data = {
-    meetUp: [
+    post: [
         { icon: Delete, value: '게시글 삭제하기', type: 'delete' },
         { icon: Edit, value: '게시글 수정하기', type: 'edit' },
     ],
