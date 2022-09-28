a2e98db73e3bddce68a4f393769def282031320a
YDayeon <88532969+YDayeon@users.noreply.github.com>
2022-09-28 09:10:25 +0900

    [컴포넌트] API fetch 함수 추가

--- src/pages/new-post/new-post.js
+++ src/pages/new-post/new-post.js
@@ -1,12 +1,20 @@
+import axios from 'axios'
 import React from 'react'
+import useForm from '../../hooks/useForm'
 import useInput from '../../hooks/useInput'
 import NewPostTemplate from './new-post-template'
 
 function NewPost() {
-    const [value, handleChange] = useInput()
+    const [value] = useInput()
+    const { form, handleChange } = useForm()
+    let token =
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2MzBmNTMwNGM2ODU3MTE5M2MxZDhiNzIiLCJleHAiOjE2NjM3NTYyMDEsImlhdCI6MTY2MzU4MzQwMSwiZW1haWwiOiJndWVzdDIyMkBnbWFpbC5jb20ifQ.2beQHnvB5iE7Cw4-fSruKX_8OLafB6a3VHAPTKXeNHwETohZHQzQFtMV-8HHRhrYTVpsM-dI5DDMVhBiKm576Q'
+    const config = { headers: { Authorization: token } }
 
-    function submit() {
-        return
+    function handleSubmit() {
+        axios
+            .post(`api/posts/create`, form, config)
+            .then((res) => console.log('new-post-form=====>', res))
     }
 
     return (
@@ -15,7 +23,7 @@ function NewPost() {
             text={'Albert Flores'}
             value={value}
             handleChange={handleChange}
-            submit={submit}
+            submit={handleSubmit}
         />
     )
 }
