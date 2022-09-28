f7ef28d1bc44c41dec12250e2ab0927be7fbeae6
YDayeon <88532969+YDayeon@users.noreply.github.com>
2022-09-28 17:47:47 +0900

    [컴포넌트] CSS 수정

--- src/components/atoms/tags/meet-up-tag/index.js
+++ src/components/atoms/tags/meet-up-tag/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
-import useMeetUpTag from './useMeetUpTag'
+import useMeetUpTag from '../../../../hooks/useMeetUpTag'
 
 const Button = styled.button`
-    height: 23px;
+    height: 24px;
     color: white;
     font-family: 'NotoSansKR';
-    width: 90px;
-    margin-right: 5px;
-    background-color: ${(props) => props.bgColor};
+    width: 100px;
+    background-color: ${(props) => props.backgroundColor};
     border: 2px solid #414042;
-    border-radius: 15px;
+    border-radius: 12px;
     display: flex;
+    font-size: 14px;
     justify-content: center;
     align-items: center;
 `
@@ -20,7 +20,7 @@ function MeetUpTag({ code, displayName = '', handleClick }) {
     const [bgColor, text] = useMeetUpTag(code, displayName)
 
     return (
-        <Button onClick={handleClick} bgColor={bgColor} name={code}>
+        <Button onClick={handleClick} backgroundColor={bgColor} name={code}>
             {text}
         </Button>
     )
