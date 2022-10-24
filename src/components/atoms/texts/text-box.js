39fe9d5bff836d4c66e8c8bf33afcfbfe9a42224
Dayeon Yoon <yday1223@gmail.com>
2022-10-24 23:57:57 +0900

    [컴포넌트] html 줄바꿈 css 수정

--- src/components/atoms/texts/text-box.js
+++ src/components/atoms/texts/text-box.js
@@ -5,6 +5,7 @@ const StyledP = styled.p`
     font-family: 'Roboto';
     font-size: ${(props) => (props.size === 'big' ? '15px' : '12px')};
     margin: 10px 0;
+    white-space: pre;
     mark {
         background-color: transparent;
         color: coral;
